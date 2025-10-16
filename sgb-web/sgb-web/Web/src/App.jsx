f8ae8d48a31f0d43155ea3e9e6ced3aa223d3eb0
NickCampos03 <nicolasjdc45@gmail.com>
2025-10-16 20:43:00 -0300

    Resolved local changes/conflict

--- sgb-web/sgb-web/Web/src/App.jsx
+++ sgb-web/sgb-web/Web/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+<img src="/vite.svg" alt="Vite logo" />
 import './App.css'
 import Login from './RealizarLogin';
 import SideMenu from './SideMenu';

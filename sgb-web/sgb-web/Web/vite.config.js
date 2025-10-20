6963358c19bc3998cb322f32121f20e37a534de9
NickCampos03 <nicolasjdc45@gmail.com>
2025-10-19 21:19:50 -0300

    Atualizacao 19102025

--- sgb-web/sgb-web/Web/vite.config.js
+++ sgb-web/sgb-web/Web/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Configuração do proxy
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/genero': 'http://localhost:8080',
+      '/emprestimos': 'http://localhost:8080',
+      '/usuarios': 'http://localhost:8080',
+      '/livros': 'http://localhost:8080',
+      '/login': 'http://localhost:8080', 
+    },
+  },
 })
